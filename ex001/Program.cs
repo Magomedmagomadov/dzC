18fe34b708423ce323dc5a9ab7a11bc815d091f6
Magomed Magomadov <magomed.magomadov_124@mail.ru>
2022-09-23 02:22:39 +0300

    Задача 2

--- ex001/Program.cs
+++ ex001/Program.cs
@@ -1,2 +1,18 @@
-﻿
-Console.WriteLine("2 Hello, World!");
+﻿Console.Clear();
+
+Console.Write("Введите число 1: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if (n > max) max = n;
+if (m > max) max = m;
+Console.WriteLine("max= ");
+Console.WriteLine(max);
+
+    
+/*Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
