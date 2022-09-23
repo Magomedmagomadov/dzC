d63efc3ac63cca92fefacc42d0ba3e63bac64fc2
Magomed Magomadov <magomed.magomadov_124@mail.ru>
2022-09-23 03:03:28 +0300

    Задача 4

---
+++ ex002/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.WriteLine("Введите 1-е число: ");
+    int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2-е число: ");
+    int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 3-е число: ");
+    int c = Convert.ToInt32(Console.ReadLine());
+    int max = a;
+ if ( b > max)
+    max = b;
+if (c > max)
+    max = c;
+    Console.WriteLine(max);
+
+/*Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
