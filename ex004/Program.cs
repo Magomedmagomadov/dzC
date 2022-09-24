c5973c5e667657f55b3086f2de641b436ac6156d
Magomed Magomadov <magomed.magomadov_124@mail.ru>
2022-09-24 08:34:13 +0300

    В папке 004 задача 6

---
+++ ex004/Program.cs
@@ -0,0 +1,15 @@
+﻿/*  Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет*/
+
+
+Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n % 2 == 0)
+    Console.WriteLine("Yes");
+else
+    Console.WriteLine("no " + n % 2);
+ 
